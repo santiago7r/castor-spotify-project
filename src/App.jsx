c598f61207fa235a3233d27803cc7c2d64751bed
Eddy Rhenals <eddy.rhenals7@gmail.com>
2024-07-10 16:58:43 -0500

    Create repo and form for the search

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [track, setTrack] = useState('');
+
+  const handleSearch = e => {
+    e.preventDefault();
+    if(track.trim() === '') {
+      alert('You should type something');
+      return
+    }
+    console.log(track);
+    setTrack('');
+  }
+
+  return (
+    <>
+      <h1>Spotify Castor</h1>
+      <form onSubmit={handleSearch}>
+        <input type="text" value={track} onChange={e => setTrack(e.target.value)} />
+        <button type='submit'>Search</button>
+      </form>
+    </>
+  )
+}
+
+export default App
