c31c0f63942836f1859376d665325d03ced7e94e
Eddy Rhenals <eddy.rhenals7@gmail.com>
2024-07-15 19:43:16 -0500

    started logic for click for details

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,17 +1,21 @@
 /* eslint-disable react/prop-types */
+import { useState } from 'react';
 import './Card.css';
+import Details from '../Details';
 
 const Card = ({ data }) => {
+    const [showDetails, setShowDetails] = useState(false)
     const { img, mainText ="Main Text", subText="Sub Text" } = data;
 
     const handleOnClick = () => {
-        console.log("CLICK ON the img")
+        setShowDetails(!showDetails)
     }
     
     return (
         <div>
             <div onClick={handleOnClick}>
                 <img className="main-img" src={img} alt="" />
+                {showDetails ? <Details /> : ''}
             </div>
             <div>
                 <span className="main-text"> {mainText} </span>
--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,7 +1,8 @@
 
 
-
 const Details = () => {
+
+  return <div>THIS ARE THE DETAILS OF A SONG</div>
 }
 
 export default Details
