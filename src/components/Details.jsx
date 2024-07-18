aaa3b99e3a02c0969f0ba4f804b94b2ad9057990
Eddy Rhenals <eddy.rhenals7@gmail.com>
2024-07-17 19:00:45 -0500

    Add total tracks to Albums

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -32,8 +32,15 @@ export default function MediaControlCard() {
   const { type, id } = useParams()
   const { data } = useGetItemData({ type, id })
   console.log('spotify data', data)
-  let image, mainText
-  if (data && (type === 'albums' || type === 'artists')) {
+
+  let image, mainText, subText
+
+  if (data && type === 'albums' || type === 'artists') {
+    image = data.images[0].url
+    mainText = data.name
+    subText = `Total tracks: ${data.total_tracks}`
+  }
+  if (data && type === 'artists') {
     image = data.images[0].url
     mainText = data.name
   }
@@ -58,6 +65,9 @@ export default function MediaControlCard() {
           <CardContent sx={{ flex: '1 0 auto' }}>
             <Typography component="div" variant="h5">
               {mainText}
+              <Box>
+                {subText}
+              </Box>
             </Typography>
             <Typography variant="subtitle1" color="text.secondary" component="div">
               {}
